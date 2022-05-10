2aaaee879048f3134d902195d53d987b15283f47
zwl <zwl@163.com>
2022-05-10 21:37:19 +0800

    添加service-hosp服务

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/ServiceHospApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ServiceHospApplication
+ * @Description TODO
+ * @Author zwl
+ * @Date 2022/5/10 21:21
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
